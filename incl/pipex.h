d274d25a0c849a87a33f327f81af1c4c23f0b518
Lauri Litovuo <llitovuo@student.hive.fi>
2024-02-08 17:57:43 +0200

    updating header to match the prototypes

--- incl/pipex.h
+++ incl/pipex.h
@@ -6,7 +6,7 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 14:42:16 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/02/07 17:40:55 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/02/08 16:17:34 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,17 @@ typedef struct s_pipex
 	int				here_doc; // for bonus
 }				t_pipex;
 
+char	**get_paths(char ***cmds, int cmd_count, char **envp);
 char	**parse_path(char **envp);
-char	**parse_cmd(char **av);
-char	*find_path(char **paths, char *cmd);
+char	*find_path(char *cmd, char **env_paths);
+
+int		count_cmds(int ac, char **av);
+char	***get_cmds(char **av, int cmd_count);
+
+void	free_2d_arr(char **arr);
+void	free_3d_arr(char ***arr);
+void	free_struct(t_pipex *cont);
+
 char	*join_str(char *path, char *cmd);
 void	error_handling(int errcode, char **paths);
 void	check_permissions(char **av);
