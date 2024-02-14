6d906c7c3eecd6a4fdc4282f740336ac0cf47763
Lauri Litovuo <llitovuo@student.hive.fi>
2024-02-14 15:53:57 +0200

    working prototype of piping the commands

--- srcs/piping.c
+++ srcs/piping.c
@@ -6,22 +6,11 @@
 /*   By: llitovuo <llitovuo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 12:55:51 by llitovuo          #+#    #+#             */
-/*   Updated: 2024/02/13 14:34:13 by llitovuo         ###   ########.fr       */
+/*   Updated: 2024/02/14 15:12:38 by llitovuo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/pipex.h"
-
-/**
- * @brief 
- * 
- *  fd[1], write ====== fd[0], read
- * 
- * @param cont 
- * @param fd 
- * @param i 
- * @return int 
- */
 
 int	dup_and_exec(t_pipex *cont, int input_fd, int output_fd, int i)
 {
@@ -29,21 +18,20 @@ int	dup_and_exec(t_pipex *cont, int input_fd, int output_fd, int i)
 		return (-1);
 	if (dup2(input_fd, STDIN_FILENO) == -1)
 	{
-		perror("dup failed");
+		perror("input dup failed");
 		return (-1);
 	}
 	if (dup2(output_fd, STDOUT_FILENO) == -1)
 	{
-		perror("dup failed");
+		perror("output dup failed");
 		return (-1);
 	}
-	if (execve(cont->paths[i], cont->cmds[i][0], NULL) == -1)
+	if (execve(cont->paths[i], &cont->cmds[i][0], NULL) == -1)
 		perror("execve failed");
 	return (-1);
 }
 
-
-int	infile_pipe(t_pipex *cont, int *fd, int i)
+int	infile_into_pipe(t_pipex *cont, int *fd, int i)
 {
 	pid_t	pid;
 
@@ -57,7 +45,7 @@ int	infile_pipe(t_pipex *cont, int *fd, int i)
 	{
 		if (close(fd[0]) == -1)
 		{
-			perror("close failed");
+			perror("infile close failed");
 			return (-1);
 		}
 		if (dup_and_exec(cont, cont->fd_in, fd[1], i) == -1)
@@ -71,7 +59,7 @@ int	infile_pipe(t_pipex *cont, int *fd, int i)
 	return (pid);
 }
 
-int	outfile_pipe(t_pipex *cont, int *fd, int i)
+int	pipe_into_outfile(t_pipex *cont, int *fd, int i)
 {
 	pid_t	pid;
 
@@ -83,48 +71,15 @@ int	outfile_pipe(t_pipex *cont, int *fd, int i)
 	}
 	if (pid == 0)
 	{
-		if (close(fd[1]) == -1)
-		{
-			perror("close failed");
-			return (-1);
-		}
+		close (fd[1]);
 		if (dup_and_exec(cont, fd[0], cont->fd_out, i) == -1)
 			return (-1);
 	}
 	else
 	{
-		close(cont->fd_out);
 		close(fd[0]);
+		close(cont->fd_out);
 	}
-	return (pid);
-}
-
-int	cmd_cmd_pipe(t_pipex *cont, int *fd, int i)
-{
-	pid_t	pid;
-	int		fd2[2]; // fd[1], write ====== fd[0], read
-
-	if (pipe(fd2) < 0)
-	{
-		perror ("pipe failed");
-		return (-1);
-	}
-	pid = fork();
-	if (pid == -1)
-	{
-		perror ("fork failed");
-		return (-1);
-	}
-	if (pid == 0)
-	{
-		close (fd2[0]);
-		close (fd[1]);
-		dup_and_exec(cont, fd[0], fd2[1], i);
-	}
-	dup2 (fd2[0], fd[0]);
-	dup2 (fd2[1], fd[1]);
-	close (fd2[0]);
-	close (fd2[1]);
 	return (pid);
 }
 
@@ -140,20 +95,13 @@ int	piping(t_pipex *cont, int **pids)
 		perror("pipe failed");
 		return (-1);
 	}
-	(*pids)[i] = infile_pipe(cont, fd, i);
+	(*pids)[i] = infile_into_pipe(cont, fd, i);
 	if ((*pids)[i] == -1)
 		return (-1);
-	while (i < cont->cmd_count)
-	{
-		i++;
-		(*pids)[i] = cmd_cmd_pipe(cont, fd, i);
-		if ((*pids)[i] == -1)
-			return (-1);
-	}
-	(*pids)[i] = outfile_pipe(cont, fd, i);
+	i++;
+	(*pids)[i] = pipe_into_outfile(cont, fd, i);
 	if ((*pids)[i] == -1)
 		return (-1);
 	(*pids)[++i] = 0;
 	return (0);
 }
-
